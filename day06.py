da4ca740fece8cf6c632bede5ee12503b7b53f91
lisaelsi <lisasamuelssons@gmail.com>
2022-12-06 12:55:18 +0100

    added prints

--- day06.py
+++ day06.py
@@ -1,14 +1,13 @@
 with open('input/day06input.txt') as f:
     lines = f.readlines()
 
-
 characters = lines[0]
 
-# PART 2
+# PART 1
 for i in range(1, len(characters)):
     four = characters[i:i+4]
     if(len(set(four)) == 4):
-        print(4+i)
+        print('First positions of start-of-message markers (4 distinct characters): {}'.format(4+i))
         break
 
 
@@ -16,7 +15,7 @@ for i in range(1, len(characters)):
 for i in range(1, len(characters)):
     fourteen = characters[i:i+14]
     if(len(set(fourteen)) == 14):
-        print(14+i)
+        print('first positions of start-of-message markers (14 distinct characters): {}'.format(14+i))
         break
 
 
